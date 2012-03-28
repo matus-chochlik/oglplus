0592ebb633a6e47b3bbb4d006b262b1aa910c917
Matus Chochlik <chochlik@gmail.com>
2012-03-28 15:59:49 +0200

    Multiple changes
    - Added a new shape builder making shapes by revolving an segmented
      line around the y-axis full circle
    - Added an example showing the usage of this shape builder
    - Minor changes in the build system
    - Documentation typo fixes

--- include/oglplus/shapes/revolve.hpp
+++ include/oglplus/shapes/revolve.hpp
@@ -1,49 +1,84 @@
 /**
- *  @file oglplus/shapes/torus.hpp
- *  @brief Torus builder
+ *  @file oglplus/shapes/revolve.hpp
+ *  @brief Shape builders revolving splines in full circles
  *
  *  @author Matus Chochlik
  *
- *  Copyright 2010-2011 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2010-2012 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#ifndef OGLPLUS_SHAPES_TORUS_1107121519_HPP
-#define OGLPLUS_SHAPES_TORUS_1107121519_HPP
+#ifndef OGLPLUS_SHAPES_REVOLVE_1107121519_HPP
+#define OGLPLUS_SHAPES_REVOLVE_1107121519_HPP
 
 #include <oglplus/shapes/draw.hpp>
 #include <oglplus/shapes/vert_attr_info.hpp>
 #include <oglplus/face_mode.hpp>
+
+#include <oglplus/vector.hpp>
+#include <oglplus/matrix.hpp>
 
 #include <cmath>
 
 namespace oglplus {
 namespace shapes {
 
-/// Class providing vertex attributes and instructions for rendering of a Torus
-class Torus
+/// Class providing attributes and instructions for rendering of a revolved shape
+template <typename Type>
+class RevolveY
  : public DrawingInstructionWriter
 {
 private:
-	GLdouble _radius_out, _radius_in;
-	size_t _sections, _rings;
-public:
-	/// Creates a torus with unit radius centered at the origin
-	Torus(void)
-	 : _radius_out(1.0)
-	 , _radius_in(0.5)
-	 , _sections(36)
-	 , _rings(24)
-	{ }
+	const std::vector<Vector<Type, 3>> _positions;
+	std::vector<Vector<Type, 3>> _normals;
+	std::vector<Vector<Type, 3>> _tex_coords;
+	const size_t _sections, _rings;
 
-	/// Creates a torus with unit radius centered at the origin
-	Torus(GLdouble rad_out, GLdouble rad_in, size_t sects, size_t rings)
-	 : _radius_out(rad_out)
-	 , _radius_in(rad_in)
-	 , _sections(sects)
-	 , _rings(rings)
-	{ }
+	void _calculate_normals(void)
+	{
+		assert(_normals.empty());
+		_normals.resize(_positions.size());
+		const size_t n = _normals.size()-1;
+		const Vec3f tangent(0.0, 0.0, -1.0);
+		_normals[0] = Normalized(Cross(
+			tangent,
+			_positions[1] - _positions[0]
+		));
+		for(size_t i=1; i!=n; ++i)
+		{
+			_normals[i] = Normalized(Cross(
+				tangent,
+				_positions[i+1] - _positions[i-1]
+			));
+		}
+		_normals[n] = Normalized(Cross(
+			tangent,
+			_positions[n] - _positions[n-1]
+		));
+	}
+
+	void _check_and_init(void)
+	{
+		assert(_rings > 1);
+		assert(_sections > 2);
+		if(_normals.empty()) _calculate_normals();
+	}
+public:
+	/// Creates a shape by revolving curve approximation around the y-axis
+	RevolveY(
+		size_t sections,
+		const std::vector<Vector<Type, 3>>& positions,
+		const std::vector<Vector<Type, 3>>& normals,
+		const std::vector<Vector<Type, 3>>& tex_coords
+	): _positions(positions)
+	 , _normals(normals)
+	 , _tex_coords(tex_coords)
+	 , _sections(sections)
+	 , _rings(_positions.size())
+	{
+		_check_and_init();
+	}
 
 	/// Returns the winding direction of faces
 	FaceOrientation FaceWinding(void) const
@@ -55,25 +90,24 @@ public:
 	template <typename T>
 	GLuint Positions(std::vector<T>& dest) const
 	{
-		dest.resize((_rings + 1) * (_sections + 1) * 3);
+		dest.resize((_rings) * (_sections + 1) * 3);
 		size_t k = 0;
 		//
-		GLdouble r_step = (2.0 * M_PI) / GLdouble(_rings);
-		GLdouble s_step = (2.0 * M_PI) / GLdouble(_sections);
-		GLdouble r1 = _radius_in;
-		GLdouble r2 = _radius_out - _radius_in;
+		const GLdouble s_step = (1.0) / GLdouble(_sections);
 
-		for(size_t r=0; r!=(_rings+1); ++r)
+		for(size_t s=0; s!=(_sections+1); ++s)
 		{
-			GLdouble vx = std::cos(r*r_step);
-			GLdouble vz = std::sin(r*r_step);
-			for(size_t s=0; s!=(_sections+1); ++s)
+			const auto angle = FullCircles(s*s_step);
+			const auto mat = ModelMatrix<Type>::RotationY(angle);
+
+			for(size_t r=0; r!=_rings; ++r)
 			{
-				GLdouble vr = std::cos(s*s_step);
-				GLdouble vy = std::sin(s*s_step);
-				dest[k++] = vx*(r1 + r2 * (1.0 + vr));
-				dest[k++] = vy*r2;
-				dest[k++] = vz*(r1 + r2 * (1.0 + vr));
+				const Vector<Type, 4> in(_positions[r], 1.0);
+				const Vector<Type, 4> out = mat * in;
+
+				dest[k++] = out.x();
+				dest[k++] = out.y();
+				dest[k++] = out.z();
 			}
 		}
 		assert(k == dest.size());
@@ -84,23 +118,24 @@ public:
 	template <typename T>
 	GLuint Normals(std::vector<T>& dest) const
 	{
-		dest.resize((_rings + 1) * (_sections + 1) * 3);
+		dest.resize((_rings) * (_sections + 1) * 3);
 		size_t k = 0;
 		//
-		GLdouble r_step = (2.0 * M_PI) / GLdouble(_rings);
-		GLdouble s_step = (2.0 * M_PI) / GLdouble(_sections);
+		const GLdouble s_step = (1.0) / GLdouble(_sections);
 
-		for(size_t r=0; r!=(_rings+1); ++r)
+		for(size_t s=0; s!=(_sections+1); ++s)
 		{
-			GLdouble vx = std::cos(r*r_step);
-			GLdouble vz = std::sin(r*r_step);
-			for(size_t s=0; s!=(_sections+1); ++s)
+			const auto angle = FullCircles(s*s_step);
+			const auto mat = ModelMatrix<Type>::RotationY(angle);
+
+			for(size_t r=0; r!=_rings; ++r)
 			{
-				GLdouble vr = std::cos(s*s_step);
-				GLdouble vy = std::sin(s*s_step);
-				dest[k++] = vx*vr;
-				dest[k++] = vy;
-				dest[k++] = vz*vr;
+				const Vector<Type, 4> in(_normals[r], 0.0);
+				const Vector<Type, 4> out = mat * in;
+
+				dest[k++] = out.x();
+				dest[k++] = out.y();
+				dest[k++] = out.z();
 			}
 		}
 		assert(k == dest.size());
@@ -111,21 +146,24 @@ public:
 	template <typename T>
 	GLuint Tangents(std::vector<T>& dest) const
 	{
-		dest.resize((_rings + 1) * (_sections + 1) * 3);
+		dest.resize((_rings) * (_sections + 1) * 3);
 		size_t k = 0;
 		//
-		GLdouble r_step = (2.0 * M_PI) / GLdouble(_rings);
-		GLdouble s_step = (2.0 * M_PI) / GLdouble(_sections);
+		GLdouble s_step = (1.0) / GLdouble(_sections);
 
-		for(size_t r=0; r!=(_rings+1); ++r)
+		const Vector<Type, 4> in(0.0, 0.0, -1.0, 0.0);
+
+		for(size_t s=0; s!=(_sections+1); ++s)
 		{
-			GLdouble vx = std::cos(r*r_step);
-			GLdouble vz = std::sin(r*r_step);
-			for(size_t s=0; s!=(_sections+1); ++s)
+			const auto angle = FullCircles(s*s_step);
+			const auto mat = ModelMatrix<Type>::RotationY(angle);
+			const auto out = mat * in;
+
+			for(size_t r=0; r!=_rings; ++r)
 			{
-				dest[k++] = +vz;
-				dest[k++] = T(0);
-				dest[k++] = -vx;
+				dest[k++] = out.x();
+				dest[k++] = out.y();
+				dest[k++] = out.z();
 			}
 		}
 		assert(k == dest.size());
@@ -136,24 +174,27 @@ public:
 	template <typename T>
 	GLuint TexCoordinates(std::vector<T>& dest) const
 	{
-		dest.resize((_rings + 1) * (_sections + 1) * 2);
+		dest.resize((_rings) * (_sections + 1) * 3);
 		size_t k = 0;
 		//
-		GLdouble r_step = 1.0 / GLdouble(_rings);
-		GLdouble s_step = 1.0 / GLdouble(_sections);
+		GLdouble s_step = (1.0) / GLdouble(_sections);
 
-		for(size_t r=0; r!=(_rings+1); ++r)
+		const Vector<Type, 4> in(0.0, 0.0, -1.0, 0.0);
+
+		for(size_t s=0; s!=(_sections+1); ++s)
 		{
-			GLdouble u = r*r_step;
-			for(size_t s=0; s!=(_sections+1); ++s)
+			const T u_mult = s*s_step;
+			const auto angle = FullCircles(u_mult);
+
+			for(size_t r=0; r!=_rings; ++r)
 			{
-				GLdouble v = s*s_step;
-				dest[k++] = u;
-				dest[k++] = v;
+				dest[k++] = _tex_coords[r].x()*u_mult;
+				dest[k++] = _tex_coords[r].y();
+				dest[k++] = _tex_coords[r].z();
 			}
 		}
 		assert(k == dest.size());
-		return 2;
+		return 3;
 	}
 
 #if OGLPLUS_DOCUMENTATION_ONLY
@@ -165,10 +206,10 @@ public:
 	 *  - "Tangent" the vertex tangent vector (Tangents)
 	 *  - "TexCoord" the ST texture coordinates (TexCoordinates)
 	 */
-	typedef VertexAttribsInfo<Torus> VertexAttribs;
+	typedef VertexAttribsInfo<RevolveY> VertexAttribs;
 #else
 	typedef VertexAttribsInfo<
-		Torus,
+		RevolveY,
 		VertexPositionsTag,
 		VertexNormalsTag,
 		VertexTangentsTag,
@@ -182,21 +223,21 @@ public:
 	/// Returns element indices that are used with the drawing instructions
 	IndexArray Indices(void) const
 	{
-		const size_t n = 2 * (_rings)*(_sections + 1);
+		const size_t n = 2 * (_rings)*(_sections);
 		assert((1<<(sizeof(GLushort)*8)) - 1 >= n);
 		//
 		IndexArray indices(n);
 		size_t k = 0;
 		size_t offs = 0;
 		// the triangle strips
-		for(size_t r=0; r!=(_rings); ++r)
+		for(size_t s=0; s!=_sections; ++s)
 		{
-			for(size_t s=0; s!=(_sections+1); ++s)
+			for(size_t r=0; r!=_rings; ++r)
 			{
-				indices[k++] = offs + s;
-				indices[k++] = offs + s + (_sections+1);
+				indices[k++] = offs + r + _rings;
+				indices[k++] = offs + r;
 			}
-			offs += _sections + 1;
+			offs += _rings;
 		}
 		assert(k == indices.size());
 		//
@@ -205,6 +246,7 @@ public:
 	}
 
 	/// Returns element indices that are used with the drawing instructions
+/* TODO
 	IndexArray IndicesWithAdjacency(void) const
 	{
 		const size_t m = (_rings)*(_sections + 1);
@@ -235,20 +277,22 @@ public:
 		// return the indices
 		return std::move(indices);
 	}
+*/
 
 	/// Returns the instructions for rendering
 	DrawingInstructions Instructions(void) const
 	{
 		auto instructions = this->MakeInstructions();
-		for(size_t r=0; r!=_rings; ++r)
+		size_t step = _rings * 2;
+		for(size_t s=0; s!=_sections; ++s)
 		{
 			this->AddInstruction(
 				instructions,
 				{
 					DrawOperation::Method::DrawElements,
 					PrimitiveType::TriangleStrip,
-					GLuint(r * (_sections + 1) * 2),
-					GLuint((_sections + 1) * 2),
+					GLuint(s * step),
+					GLuint(step),
 					0
 				}
 			);
@@ -257,6 +301,7 @@ public:
 	}
 
 	/// Returns the instructions for rendering
+/* TODO
 	DrawingInstructions InstructionsWithAdjacency(void) const
 	{
 		auto instructions = this->MakeInstructions();
@@ -275,6 +320,7 @@ public:
 		}
 		return std::move(instructions);
 	}
+*/
 };
 
 } // shapes
