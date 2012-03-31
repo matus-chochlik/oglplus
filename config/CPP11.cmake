9c9918f3b48c07dd2e264266ac32d4edfb087f55
Matus Chochlik <chochlik@gmail.com>
2012-03-31 12:14:30 +0200

    Backporting to MSVC 11
    - Update of the cmake build/configuration system

--- config/CPP11.cmake
+++ config/CPP11.cmake
@@ -6,3 +6,25 @@
 # we need C++11
 # TODO add support for various compilers
 add_definitions(-std=c++0x)
+
+function(cpp11_feature_detection FEATURE_NAME)
+	configure_file(
+		${PROJECT_SOURCE_DIR}/config/cpp11/has_${FEATURE_NAME}.cpp
+		${PROJECT_BINARY_DIR}/cpp11/has_${FEATURE_NAME}.cpp
+	)
+
+	try_compile(
+		OGLPLUS_HAS_${FEATURE_NAME}
+		${PROJECT_BINARY_DIR}/cpp11
+		${PROJECT_BINARY_DIR}/cpp11/has_${FEATURE_NAME}.cpp
+	)
+	message(STATUS "Detecting support for c++11 feature '${FEATURE_NAME}': ${OGLPLUS_HAS_${FEATURE_NAME}}")
+	if(OGLPLUS_HAS_${FEATURE_NAME})
+		set(OGLPLUS_NO_${FEATURE_NAME} 0 PARENT_SCOPE)
+	else()
+		set(OGLPLUS_NO_${FEATURE_NAME} 1 PARENT_SCOPE)
+	endif()
+	unset(OGLPLUS_HAS_${FEATURE_NAME})
+endfunction()
+
+cpp11_feature_detection(NULLPTR)
