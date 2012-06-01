776a661d352d649373e37a1845dd955969137649
Matus Chochlik <chochlik@gmail.com>
2012-06-01 15:08:04 +0200

    Multiple changes
    - Added a new Icosahedron shape builder
    - Added a new example

---
+++ example/oglplus/017_single_pass_edges.cpp
@@ -0,0 +1,265 @@
+/**
+ *  @example oglplus/017_single_pass_edges.cpp
+ *  @brief Shows how render the faces and the wireframe in single pass
+ *
+ *  @image html 017_single_pass_edges.png
+ *
+ *  Copyright 2008-2012 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#include <oglplus/gl.hpp>
+#include <oglplus/all.hpp>
+#include <oglplus/shapes/icosahedron.hpp>
+
+#include "example.hpp"
+
+namespace oglplus {
+
+class IcosahedronExample : public Example
+{
+private:
+	// helper object building shape vertex attributes
+	shapes::Icosahedron make_shape;
+	// helper object encapsulating shape drawing instructions
+	shapes::DrawingInstructions shape_instr;
+	// indices pointing to shape primitive elements
+	shapes::Icosahedron::IndexArray shape_indices;
+
+	// wrapper around the current OpenGL context
+	Context gl;
+
+	// Vertex shader
+	VertexShader vs;
+
+	// Geometry shader
+	GeometryShader gs;
+
+	// Fragment shader
+	FragmentShader fs;
+
+	// Program
+	Program prog;
+
+	// A vertex array object for the rendered shape
+	VertexArray shape;
+
+	// VBOs for the shape's vertices and element indices
+	Buffer verts, indices;
+public:
+	IcosahedronExample(void)
+	 : shape_instr(make_shape.Instructions())
+	 , shape_indices(make_shape.Indices())
+	 , vs("Vertex")
+	 , gs("Geometry")
+	 , fs("Fragment")
+	{
+		vs.Source(
+			"#version 330\n"
+
+			"const vec3 LightPosition = vec3(10.0, 10.0, 7.0);"
+
+			"uniform mat4 ProjectionMatrix, CameraMatrix, ModelMatrix;"
+
+			"in vec4 Position;"
+
+			"out vec3 vertNormal;"
+			"out vec3 vertLightDir;"
+
+			"void main(void)"
+			"{"
+			"	gl_Position = "
+			"		ModelMatrix *"
+			"		Position;"
+			"	vertNormal = ("
+			"		ModelMatrix *"
+			"		vec4(Position.xyz, 0.0)"
+			"	).xyz;"
+			"	vertLightDir = "
+			"		LightPosition -"
+			"		gl_Position.xyz;"
+			"	gl_Position = "
+			"		ProjectionMatrix *"
+			"		CameraMatrix *"
+			"		gl_Position;"
+			"}"
+		);
+		vs.Compile();
+
+		gs.Source(
+			"#version 330\n"
+			"layout (triangles) in;"
+			"layout (triangle_strip, max_vertices = 3) out;"
+
+			"uniform vec2 ViewportDimensions;"
+
+			"in vec3 vertNormal[], vertLightDir[];"
+
+			"noperspective out vec3 geomDist;"
+			"flat out vec3 geomNormal;"
+			"flat out vec3 geomColor;"
+			"out vec3 geomLightDir;"
+
+			"void main(void)"
+			"{"
+			"	geomNormal = normalize("
+			"		vertNormal[0]+"
+			"		vertNormal[1]+"
+			"		vertNormal[2]"
+			"	);"
+			"	geomColor = normalize(abs("
+			"		vec3(1.0, 1.0, 1.0)-"
+			"		geomNormal"
+			"	));"
+
+			"	vec2 ScreenPos[3];"
+			"	for(int i=0; i!=3; ++i)"
+			"	{"
+			"		ScreenPos[i] = "
+			"			ViewportDimensions*"
+			"			gl_in[i].gl_Position.xy/"
+			"			gl_in[i].gl_Position.w;"
+			"	}"
+
+			"	vec2 TmpVect[3];"
+			"	for(int i=0; i!=3; ++i)"
+			"	{"
+			"		TmpVect[i] = "
+			"			ScreenPos[(i+2)%3]-"
+			"			ScreenPos[(i+1)%3];"
+			"	}"
+
+			"	const vec3 EdgeMask[3] = vec3[3]("
+			"		vec3(1.0, 0.0, 0.0),"
+			"		vec3(0.0, 1.0, 0.0),"
+			"		vec3(0.0, 0.0, 1.0) "
+			"	);"
+
+			"	for(int i=0; i!=3; ++i)"
+			"	{"
+			"		float Dist = abs("
+			"			TmpVect[(i+1)%3].x*TmpVect[(i+2)%3].y-"
+			"			TmpVect[(i+1)%3].y*TmpVect[(i+2)%3].x "
+			"		) / length(TmpVect[i]);"
+			"		vec3 DistVect = vec3(Dist, Dist, Dist);"
+
+			"		gl_Position = gl_in[i].gl_Position;"
+			"		geomLightDir = vertLightDir[i];"
+			"		geomDist = EdgeMask[i] * DistVect;"
+			"		EmitVertex();"
+			"	}"
+			"	EndPrimitive();"
+			"}"
+		);
+		gs.Compile();
+
+		fs.Source(
+			"#version 330\n"
+
+			"uniform float EdgeWidth;"
+
+			"noperspective in vec3 geomDist;"
+			"flat in vec3 geomNormal;"
+			"flat in vec3 geomColor;"
+			"in vec3 geomLightDir;"
+
+			"out vec3 fragColor;"
+
+			"void main(void)"
+			"{"
+			"	float MinDist = min(min(geomDist.x,geomDist.y),geomDist.z);"
+			"	float EdgeAlpha = exp2(-pow(MinDist/EdgeWidth, 2.0));"
+
+			"	const float Ambient = 0.8;"
+			"	float Diffuse = max(dot("
+			"		normalize(geomNormal),"
+			"		normalize(geomLightDir)"
+			"	), 0.0);"
+
+			"	vec3 FaceColor = geomColor * (Diffuse + Ambient);"
+			"	const vec3 EdgeColor = vec3(0.0, 0.0, 0.0);"
+
+			"	fragColor = mix(FaceColor, EdgeColor, EdgeAlpha);"
+			"}"
+		);
+		fs.Compile();
+
+		prog.AttachShader(vs);
+		prog.AttachShader(gs);
+		prog.AttachShader(fs);
+		prog.Link();
+		prog.Use();
+
+		shape.Bind();
+
+		verts.Bind(Buffer::Target::Array);
+		{
+			std::vector<GLfloat> data;
+			GLuint n_per_vertex = make_shape.Positions(data);
+			Buffer::Data(Buffer::Target::Array, data);
+			VertexAttribArray attr(prog, "Position");
+			attr.Setup(n_per_vertex, DataType::Float);
+			attr.Enable();
+
+			indices.Bind(Buffer::Target::ElementArray);
+			Buffer::Data(Buffer::Target::ElementArray, shape_indices);
+			shape_indices.clear();
+		}
+
+		//
+		gl.ClearColor(0.8f, 0.8f, 0.8f, 0.0f);
+		gl.ClearDepth(1.0f);
+		gl.Enable(Capability::DepthTest);
+
+		prog.Use();
+	}
+
+	void Reshape(size_t width, size_t height)
+	{
+		gl.Viewport(width, height);
+		Uniform<Vec2f>(prog, "ViewportDimensions").Set(
+			Vec2f(width, height)
+		);
+		Uniform<Mat4f>(prog, "ProjectionMatrix").Set(
+			CamMatrixf::PerspectiveX(
+				Degrees(48),
+				double(width)/height,
+				1, 100
+			)
+		);
+	}
+
+	void Render(double time)
+	{
+		gl.Clear().ColorBuffer().DepthBuffer();
+		//
+		Uniform<GLfloat>(prog, "EdgeWidth").Set(4.0+SineWave(time / 7.0)*3.0);
+
+		Uniform<Mat4f>(prog, "CameraMatrix").Set(
+			CamMatrixf::Orbiting(
+				Vec3f(),
+				4.5 - SineWave(time / 27)*2.0,
+				Degrees(time * 33),
+				Degrees(SineWave(time / 21.0) * 31)
+			)
+		);
+
+		Uniform<Mat4f>(prog, "ModelMatrix").Set(
+			ModelMatrixf::RotationZ(Degrees(time * 37))
+		);
+
+		shape_instr.Draw(shape_indices);
+	}
+
+	bool Continue(double time)
+	{
+		return time < 30.0;
+	}
+};
+
+std::unique_ptr<Example> makeExample(const ExampleParams& params)
+{
+	return std::unique_ptr<Example>(new IcosahedronExample);
+}
+
+} // namespace oglplus
---
+++ include/oglplus/shapes/icosahedron.hpp
@@ -0,0 +1,123 @@
+/**
+ *  @file oglplus/shapes/icosahedron.hpp
+ *  @brief Icosahedron builder
+ *
+ *  @author Matus Chochlik
+ *
+ *  Copyright 2010-2012 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef OGLPLUS_SHAPES_ICOSAHEDRON_1206011111_HPP
+#define OGLPLUS_SHAPES_ICOSAHEDRON_1206011111_HPP
+
+#include <oglplus/face_mode.hpp>
+#include <oglplus/shapes/draw.hpp>
+
+#if !OGLPLUS_NO_VARIADIC_TEMPLATES
+#include <oglplus/shapes/vert_attr_info.hpp>
+#endif
+
+namespace oglplus {
+namespace shapes {
+
+/// Class providing vertex attributes and instructions for drawing of a icosahedron
+class Icosahedron
+ : public DrawingInstructionWriter
+{
+private:
+public:
+	/// Returns the winding direction of faces
+	FaceOrientation FaceWinding(void) const
+	{
+		return FaceOrientation::CW;
+	}
+
+	typedef GLuint (Icosahedron::*VertexAttribFunc)(std::vector<GLfloat>&) const;
+
+	/// Makes the vertices and returns the number of values per vertex
+	template <typename T>
+	GLuint Positions(std::vector<T>& dest) const
+	{
+		const T _positions[12*3] = {
+			T( 0.000), T( 1.000), T( 0.000), // (A)
+			T( 0.894), T( 0.447), T( 0.000), // (B)
+			T( 0.276), T( 0.447), T( 0.851), // (C)
+			T(-0.724), T( 0.447), T( 0.526), // (D)
+			T(-0.724), T( 0.447), T(-0.526), // (E)
+			T( 0.276), T( 0.447), T(-0.851), // (F)
+			T( 0.724), T(-0.447), T( 0.526), // (G)
+			T(-0.276), T(-0.447), T( 0.851), // (H)
+			T(-0.894), T(-0.447), T( 0.000), // (I)
+			T(-0.276), T(-0.447), T(-0.851), // (J)
+			T( 0.724), T(-0.447), T(-0.526), // (K)
+			T( 0.000), T(-1.000), T( 0.000)  // (L)
+		};
+		dest.assign(_positions, _positions+12*3);
+		return 3;
+	}
+
+#if OGLPLUS_DOCUMENTATION_ONLY
+	/// Vertex attribute information for this shape builder
+	/** Icosahedron provides build functions for the following named
+	 *  vertex attributes:
+	 *  - "Position" the vertex positions (Positions)
+	 */
+	typedef VertexAttribsInfo<Icosahedron> VertexAttribs;
+#elif !OGLPLUS_NO_VARIADIC_TEMPLATES
+	typedef VertexAttribsInfo<
+		Icosahedron,
+		VertexPositionsTag,
+	> VertexAttribs;
+#endif
+
+	/// The type of the index container returned by Indices()
+	typedef std::vector<GLubyte> IndexArray;
+
+	/// Returns element indices that are used with the drawing instructions
+	IndexArray Indices(void) const
+	{
+		const GLubyte _indices[20*3] = {
+			 2,  1,  0,
+			 3,  2,  0,
+			 4,  3,  0,
+			 5,  4,  0,
+			 1,  5,  0,
+			11,  6,  7,
+			11,  7,  8,
+			11,  8,  9,
+			11,  9, 10,
+			11, 10,  6,
+			 1,  2,  6,
+			 2,  3,  7,
+			 3,  4,  8,
+			 4,  5,  9,
+			 5,  1, 10,
+			 2,  7,  6,
+			 3,  8,  7,
+			 4,  9,  8,
+			 5, 10,  9,
+			 1,  6, 10
+		};
+		return IndexArray(_indices, _indices+20*3);
+	}
+
+	/// Returns the instructions for rendering of faces
+	DrawingInstructions Instructions(void) const
+	{
+		DrawOperation operation;
+		operation.method = DrawOperation::Method::DrawElements;
+		operation.mode = PrimitiveType::Triangles;
+		operation.first = 0;
+		operation.count = 20*3;
+		operation.phase = 0;
+
+		return this->MakeInstructions(operation);
+	}
+};
+
+} // shapes
+} // oglplus
+
+#endif // include guard
