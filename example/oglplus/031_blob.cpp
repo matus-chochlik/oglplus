b00ce481bbbda07e2cea9d49ec996eac3eb91a54
Matus Chochlik <chochlik@gmail.com>
2012-07-26 14:52:41 +0200

    Update of build system
    - Annotated examples with required C++11 features

--- example/oglplus/031_blob.cpp
+++ example/oglplus/031_blob.cpp
@@ -9,6 +9,7 @@
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
  *  @oglplus_example_uses_cxx11{INITIALIZER_LISTS}
+ *  @oglplus_example_uses_cxx11{VARIADIC_TEMPLATES}
  */
 #include <oglplus/gl.hpp>
 #include <oglplus/all.hpp>
