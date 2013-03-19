aea987ffff6a6114a1b7c6cfbe6ec08ff1777c3e
Matus Chochlik <chochlik@gmail.com>
2013-03-19 18:28:36 +0100

    Multiple changes
    - Update of changelog
    - Minor update of the make_archive tools

--- tools/make_archive.bat
+++ tools/make_archive.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Copyright 2010-2012 Matus Chochlik. Distributed under the Boost
+:: Copyright 2010-2013 Matus Chochlik. Distributed under the Boost
 :: Software License, Version 1.0. (See accompanying file
 :: LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
@@ -12,6 +12,7 @@ SET oglplus_archive_items=^
 	include ^
 	utils ^
 	source ^
+	third_party ^
 	test ^
 	LICENSE_1_0.txt ^
 	README.rst ^
