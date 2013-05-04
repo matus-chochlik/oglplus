4912d7f569d39a6dc12f80b9c127f7f89c5301ee
Matus Chochlik <chochlik@gmail.com>
2013-05-04 16:36:18 +0200

    Updated building of OALplus docs
    - Updated the OALplus' doxyfile.
    - Updated and added the CMakeLists.
    - Added custom header/footer and .css file.

--- tools/publish_docs.sh
+++ tools/publish_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2008-2012 Matus Chochlik. Distributed under the Boost
+# Copyright 2008-2013 Matus Chochlik. Distributed under the Boost
 # Software License, Version 1.0. (See accompanying file
 # LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
@@ -13,7 +13,7 @@ doc_src="$(dirname $0)/../_build/doc/doxygen/html"
 doc_web="$(dirname $0)/../web"
 #
 (cd _build && make doc -j 4) &&
-(cd _build && make rebuild_doc) &&
+(cd _build && make rebuild-doc) &&
 # remove the old files
 ssh ${doc_user}@${doc_host} "rm -rf ${doc_base}/*" &&
 # tar stream and untar the new files
