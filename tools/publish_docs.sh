4bd2ad0fbbb1b36e3a071960cdec5bdeac2119b0
Matus Chochlik <chochlik@gmail.com>
2013-05-16 15:05:00 +0200

    Minor update to utility script

--- tools/publish_docs.sh
+++ tools/publish_docs.sh
@@ -6,16 +6,19 @@
 # the remote documentation host computer data
 doc_user=${USER}
 doc_host="kifri.fri.uniza.sk"
-doc_home="./public_html/oglplus"
-doc_base="${doc_home}/html"
-# local path to docs
-doc_src="$(dirname $0)/../_build/doc/doxygen/oglplus/html"
-doc_web="$(dirname $0)/../web"
-#
-(cd _build && make doc -j 4) &&
-(cd _build && make rebuild-doc) &&
-# remove the old files
-ssh ${doc_user}@${doc_host} "rm -rf ${doc_base}/*" &&
-# tar stream and untar the new files
-(cd ${doc_src} && tar -c . | ssh ${doc_user}@${doc_host} "mkdir -p ${doc_base} && cd ${doc_base} && tar -x && echo 'AddCharset UTF-8 .html' > .htaccess")
+
+for lib_name in oglplus oalplus
+do
+	doc_home="./public_html/${lib_name}"
+	doc_base="${doc_home}/html"
+	# local path to docs
+	doc_src="$(dirname $0)/../_build/doc/doxygen/${libname}/html"
+	#
+	(cd _build && make doc -j 4) &&
+	(cd _build && make rebuild-doc) &&
+	# remove the old files
+	ssh ${doc_user}@${doc_host} "rm -rf ${doc_base}/*" &&
+	# tar stream and untar the new files
+	(cd ${doc_src} && tar -c . | ssh ${doc_user}@${doc_host} "mkdir -p ${doc_base} && cd ${doc_base} && tar -x && echo 'AddCharset UTF-8 .html' > .htaccess")
+done
 
