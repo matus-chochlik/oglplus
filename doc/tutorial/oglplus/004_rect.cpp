a9031434cc54f43efc00ecbe023cf4c7061bcb9a
Matus Chochlik <chochlik@gmail.com>
2013-05-18 21:28:08 +0200

    Documentation update
    - Fixed some problems in the OGLplus tutorials.

--- doc/tutorial/oglplus/004_rect.cpp
+++ doc/tutorial/oglplus/004_rect.cpp
@@ -1,9 +1,9 @@
 /*
- *  .file doc/tutorial/004_newton_fractal.cpp
+ *  .file doc/tutorial/oglplus/004_newton_fractal.cpp
  *
  *  .author Matus Chochlik
  *
- *  Copyright 2010-2011 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2010-2013 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
