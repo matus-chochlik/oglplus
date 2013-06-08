830e2b9ff5c41eb20cdecec7ed1be627b2d6cb31
Matus Chochlik <chochlik@gmail.com>
2013-06-08 14:50:36 +0200

    Added missing include in EGLplus

--- include/eglplus/surface.hpp
+++ include/eglplus/surface.hpp
@@ -16,6 +16,7 @@
 #include <eglplus/eglfunc.hpp>
 #include <eglplus/error.hpp>
 #include <eglplus/display.hpp>
+#include <eglplus/configs.hpp>
 #include <eglplus/friend_of.hpp>
 #include <eglplus/attrib_list.hpp>
 #include <eglplus/surface_attrib.hpp>
