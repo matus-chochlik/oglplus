47557bc5587840c71262d260b3766d5466250f5e
Matus Chochlik <chochlik@gmail.com>
2013-08-14 16:19:34 +0200

    Added more functions to the library
    - A function from the ShapeWrapper.
    - A functions related to subroutine error handling.

--- include/oglplus/lib/oglplus.hpp
+++ include/oglplus/lib/oglplus.hpp
@@ -49,6 +49,7 @@
 #include <oglplus/shapes/blender_mesh.hpp>
 #include <oglplus/shapes/obj_mesh.hpp>
 #include <oglplus/shapes/draw.hpp>
+#include <oglplus/shapes/wrapper.hpp>
 
 #include <oglplus/images/image.hpp>
 #include <oglplus/images/brushed_metal.hpp>
