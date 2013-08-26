13ebbabbecbb1f912e26a0edfceb9a88eea0090d
Matus Chochlik <chochlik@gmail.com>
2013-08-26 06:54:43 +0000

    Moved some lib functions into separate files

--- include/oglplus/imports/blend_file/flattened.hpp
+++ include/oglplus/imports/blend_file/flattened.hpp
@@ -55,41 +55,6 @@ public:
 	}
 };
 
-#if !OGLPLUS_LINK_LIBRARY || defined(OGLPLUS_IMPLEMENTING_LIBRARY)
-
-OGLPLUS_LIB_FUNC
-const std::string& BlendFileFlattenedStructField::Name(void) const
-{
-	return _flat_fields->_field_names[_flat_field_index];
-}
-
-OGLPLUS_LIB_FUNC
-BlendFileStruct BlendFileFlattenedStructField::Parent(void) const
-{
-	return BlendFileStruct(
-		_sdna,
-		_flat_fields->_field_structs[_flat_field_index]
-	);
-}
-
-OGLPLUS_LIB_FUNC
-BlendFileStructField BlendFileFlattenedStructField::Field(void) const
-{
-	return BlendFileStructField(
-		_sdna,
-		_flat_fields->_field_structs[_flat_field_index],
-		_flat_fields->_field_indices[_flat_field_index]
-	);
-}
-
-OGLPLUS_LIB_FUNC
-uint32_t BlendFileFlattenedStructField::Offset(void) const
-{
-	return _flat_fields->_field_offsets[_flat_field_index];
-}
-
-#endif // OGLPLUS_LINK_LIBRARY
-
 class BlendFileFlattenedStructFieldRange
  : public BlendFileRangeTpl<
 	BlendFileFlattenedStructFieldRange,
@@ -158,64 +123,12 @@ BlendFileFlattenedStruct BlendFileStruct::Flattened(void) const
 	return BlendFileFlattenedStruct(*this);
 }
 
-#if !OGLPLUS_LINK_LIBRARY || defined(OGLPLUS_IMPLEMENTING_LIBRARY)
-
-OGLPLUS_LIB_FUNC
-BlendFileFlattenedStructFieldRange
-BlendFileFlattenedStruct::Fields(void) const
-{
-	const BlendFileSDNA::_flat_struct_info* flat_fields =
-		(_struct_index == _sdna->_invalid_struct_index())?
-		(const BlendFileSDNA::_flat_struct_info*)nullptr:
-		(_sdna->_struct_flatten_fields(_struct_index).get());
-
-	return BlendFileFlattenedStructFieldRange(
-		_sdna,
-		_struct_index,
-		flat_fields
-	);
-}
-
-OGLPLUS_LIB_FUNC
-BlendFileFlattenedStructField
-BlendFileFlattenedStruct::FieldByName(const std::string& name) const
-{
-	// this of course does not work for atomic types
-	if(_struct_index == _sdna->_invalid_struct_index())
-	{
-		std::string what("Requesting field '");
-		what.append(name);
-		what.append("' in an atomic type");
-		throw std::runtime_error(what);
-	}
-
-	const BlendFileSDNA::_flat_struct_info* flat_fields =
-		_sdna->_struct_flatten_fields(_struct_index).get();
-	assert(flat_fields);
-
-	auto pos = flat_fields->_field_map.find(&name);
-
-	if(pos == flat_fields->_field_map.end())
-	{
-		std::string what("Cannot find field '");
-		what.append(name);
-		what.append("' in flattened structure");
-		throw std::runtime_error(what);
-	}
-
-	const std::size_t flat_field_index = pos->second;
-
-	return BlendFileFlattenedStructField(
-		_sdna,
-		_struct_index,
-		flat_field_index,
-		flat_fields
-	);
-}
-
-#endif // OGLPLUS_LINK_LIBRARY
 
 } // imports
 } // oglplus
+
+#if !OGLPLUS_LINK_LIBRARY || defined(OGLPLUS_IMPLEMENTING_LIBRARY)
+#include <oglplus/imports/blend_file/flattened.ipp>
+#endif // OGLPLUS_LINK_LIBRARY
 
 #endif // include guard
