491a1fdc20d58548cd2fd45cd61e6b852c1e5d90
Matus Chochlik <chochlik@gmail.com>
2013-12-20 20:02:54 +0100

    Added the first files to a devel/testing EGL imp.

---
+++ utils/eglplus/egl/src/EGL/error.hpp
@@ -0,0 +1,16 @@
+/**
+ *  .file EGL/error.hpp
+ *  .brief EGL Error handling declarations
+ *
+ *  @author Matus Chochlik
+ *
+ *  Copyright 2012-2013 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+#ifndef OGLPLUS_EGL_ERROR_1306251555_HPP
+#define OGLPLUS_EGL_ERROR_1306251555_HPP
+
+extern EGLint eglplus_egl_ErrorCode;
+
+#endif // include guard
