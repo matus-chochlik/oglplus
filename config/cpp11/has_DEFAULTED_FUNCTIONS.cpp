a1013e3be31ec066278019e338f614dbf275f3c1
Matus Chochlik <chochlik@gmail.com>
2014-02-13 06:41:35 +0100

    Fix of C++11 defaulted function support detection

--- config/cpp11/has_DEFAULTED_FUNCTIONS.cpp
+++ config/cpp11/has_DEFAULTED_FUNCTIONS.cpp
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2010-2013 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2010-2014 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
@@ -10,6 +10,7 @@ class df
 {
 public:
 	df(void) = default;
+	df(df&&) = default;
 };
 
 int main(int argc, const char** argv)
