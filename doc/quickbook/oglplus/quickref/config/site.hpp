71a09cb47b72284b242514a530348e31e4e747a7
Matus Chochlik <chochlik@gmail.com>
2014-07-11 17:47:50 +0200

    Updated the quickbook docs

--- doc/quickbook/oglplus/quickref/config/site.hpp
+++ doc/quickbook/oglplus/quickref/config/site.hpp
@@ -4,8 +4,36 @@
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
-//[oglplus_config_site
+//[oglplus_config_site_1
 namespace oglplus {
+
+#define OGLPLUS_OPENAL_FOUND <UNSPECIFIED> /*<
+Indicates if __OpenAL is available and should be used.
+>*/
+#define OGLPLUS_PNG_FOUND <UNSPECIFIED> /*<
+Indicates if LibPNG was found and should be used.
+>*/
+#define OGLPLUS_PANGO_CAIRO_FOUND <UNSPECIFIED> /*<
+Indicates if Pango/Cairo was found and should be used.
+>*/
+
+//]
+//[oglplus_config_site_2
+
+#ifndef OGLPLUS_USE_GLCOREARB_H
+#define OGLPLUS_USE_GLCOREARB_H <UNSPECIFIED>
+#endif
+#ifndef OGLPLUS_USE_GL3_H
+#define OGLPLUS_USE_GL3_H <UNSPECIFIED>
+#endif
+#ifndef OGLPLUS_USE_GLEW
+#define OGLPLUS_USE_GLEW <UNSPECIFIED>
+#endif
+#ifndef OGLPLUS_USE_GL3W
+#define OGLPLUS_USE_GL3W <UNSPECIFIED>
+#endif
+
+#define OGLPLUS_USE_BOOST_CONFIG 1
 
 } // namespace oglplus
 //]
