13fb3354da4b8722134399d8223ae5d83c77d25e
Matus Chochlik <chochlik@gmail.com>
2014-08-04 18:47:30 +0200

    Documentation update

--- doc/quickbook/oglplus/quickref/math/angle.hpp
+++ doc/quickbook/oglplus/quickref/math/angle.hpp
@@ -11,7 +11,35 @@ template <typename T>
 class Angle
 {
 public:
+//]
+//[oglplus_math_angle_ctrs
+	Angle(void); /*<
+	Constructs a zero angle.
+	>*/
+	Angle(const Angle&);
 
+	template <typename U>
+	Angle(const Angle<U>& other); /*<
+	Constructs an angle from angle with different underlying type.
+>*/
+//]
+
+//[oglplus_math_angle_ctr_fns
+	static Angle Radians(T val_rad); /*<
+	Constructs a new angle from a value in radians.
+	>*/
+	static Angle Degrees(T val_deg); /*<
+	Constructs a new angle from a value in degrees.
+	>*/
+	static Angle ArcSin(T x); /*<
+	Constructs a new angle as the arc sine of the specified value.
+	>*/
+	static Angle ArcCos(T x);  /*<
+	Constructs a new angle as the arc sine of the specified value.
+	>*/
+//]
+
+//[oglplus_math_angle_end
 }; // class Angle
 
 } // namespace oglplus
