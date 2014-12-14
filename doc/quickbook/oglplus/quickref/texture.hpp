50103895994161d7aa671e1064b221d8213db51e
Matus Chochlik <chochlik@gmail.com>
2014-12-14 09:33:17 +0100

    Updated the quickbook docs

--- doc/quickbook/oglplus/quickref/texture.hpp
+++ doc/quickbook/oglplus/quickref/texture.hpp
@@ -166,6 +166,124 @@ class __ObjectOps<__tag_ExplicitSel, __tag_Texture>
  : public __ObjZeroOps<__tag_ExplicitSel, __tag_Texture>
 {
 public:
+	struct Property
+	{
+		typedef __TextureCompareMode CompareMode;
+
+		typedef __TextureFilter Filter;
+
+		typedef __TextureMagFilter MagFilter;
+
+		typedef __TextureMinFilter MinFilter;
+
+		typedef __TextureSwizzleCoord SwizzleCoord;
+
+		typedef __TextureSwizzle Swizzle;
+
+#if GL_VERSION_3_3 || GL_ARB_texture_swizzle
+		typedef __TextureSwizzleTuple SwizzleTuple;
+#endif
+		typedef __TextureWrapCoord WrapCoord;
+
+		typedef __TextureWrap Wrap;
+
+		typedef __OneOf<
+			__DataType,
+			__PixelDataType
+		> PixDataType;
+	};
+
+#if GL_VERSION_3_0
+	static
+	GLsizei Width(Target target, GLint level = 0); /*<
+	Returns the width of the texture image on the specified [^level].
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_WIDTH].
+	>*/
+
+	static
+	GLsizei Height(Target target, GLint level = 0); /*<
+	Returns the height of the texture image on the specified [^level].
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_HEIGHT].
+	>*/
+
+	static
+	GLsizei Depth(Target target, GLint level = 0); /*<
+	Returns the depth of the texture image on the specified [^level].
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_DEPTH].
+	>*/
+
+	static
+	__PixelDataType RedType(Target target, GLint level = 0); /*<
+	Returns the data type used to store the RED component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_RED_TYPE].
+	>*/
+
+	static
+	__PixelDataType GreenType(Target target, GLint level = 0); /*<
+	Returns the data type used to store the GREEN component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_GREEN_TYPE].
+	>*/
+
+	static
+	__PixelDataType BlueType(Target target, GLint level = 0); /*<
+	Returns the data type used to store the BLUE component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_BLUE_TYPE].
+	>*/
+
+	static
+	__PixelDataType AlphaType(Target target, GLint level = 0); /*<
+	Returns the data type used to store the ALPHA component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_ALPHA_TYPE].
+	>*/
+
+	static
+	__PixelDataType DepthType(Target target, GLint level = 0); /*<
+	Returns the data type used to store the DEPTH component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_DEPTH_TYPE].
+	>*/
+
+	static
+	GLsizei RedSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of the RED component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_RED_SIZE].
+	>*/
+
+	static
+	GLsizei GreenSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of the GREEN component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_GREEN_SIZE].
+	>*/
+
+	static
+	GLsizei BlueSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of the BLUE component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_BLUE_SIZE].
+	>*/
+
+	static
+	GLsizei AlphaSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of the ALPHA component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_ALPHA_SIZE].
+	>*/
+
+	static
+	GLsizei DepthSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of the DEPTH component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_DEPTH_SIZE].
+	>*/
+
+	static
+	GLsizei StencilSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of the STENCIL component.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_STENCIL_SIZE].
+	>*/
+
+	static
+	GLsizei SharedSize(Target target, GLint level = 0); /*<
+	Returns the actual resolution of all texture components.
+	See [glfunc GetTexLevelParameter], [glconst TEXTURE_SHARED_SIZE].
+	>*/
+#endif
 };
 
 // TODO
