9186cb90d5d776ab8aa14cbb780667df88c8673c
Matus Chochlik <chochlik@gmail.com>
2015-01-15 20:17:58 +0100

    Updated the quickbook docs

---
+++ doc/quickbook/oalplus/quickref/object/tags.hpp
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2014-2015 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+//[oalplus_object_tags
+namespace oalplus {
+namespace tag {
+
+struct Buffer { };
+struct Source { };
+
+} // namespace tag
+} // namespace oalplus
+//]
+
