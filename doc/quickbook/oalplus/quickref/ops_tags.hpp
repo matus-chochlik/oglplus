db5d37ff4a69d6d39cf2f19b73a591fd75139f3c
Matus Chochlik <chochlik@gmail.com>
2015-01-16 20:56:04 +0100

    Updated the quickbook docs

---
+++ doc/quickbook/oalplus/quickref/ops_tags.hpp
@@ -0,0 +1,18 @@
+/*
+ *  Copyright 2014-2015 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+//[oalplus_ops_tags
+namespace oalplus {
+namespace tag {
+
+struct DirectState; /*<
+Operations with ['direct state access]
+>*/
+
+} // namespace tag
+} // namespace oalplus
+//]
+
