d1a974962b3afb7357243962b2152d0dcb5ab098
Matus Chochlik <chochlik@gmail.com>
2015-01-18 17:44:36 +0100

    Minor documentation typo fix

--- include/oglplus/text/stb_truetype/font2d.hpp
+++ include/oglplus/text/stb_truetype/font2d.hpp
@@ -4,7 +4,7 @@
  *
  *  @author Matus Chochlik
  *
- *  Copyright 2011-2014 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2011-2015 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
@@ -28,7 +28,7 @@ namespace text {
 
 class STBTTFont2D;
 
-/// Wrapper arund the Sean Barrett's true type font glyph functionality
+/// Wrapper around the Sean Barrett's true type font glyph functionality
 /**
  *  This class is instantiated by STBTTFont2D class
  */
