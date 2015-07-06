008dad1da0cd68d6e5afbf2f26b504df28504c6e
Matus Chochlik <chochlik@gmail.com>
2015-07-06 21:23:59 +0200

    Minor update in the web index page

--- web/index.php
+++ web/index.php
@@ -200,7 +200,7 @@
 				<h3>Other resources</h3>
 				<ul>
 					<li><a href="https://github.com/jjzazuet/jgl" title="Java wrapper for OpenGL 3 and higher by Jesús Zazueta">JGL</a> by Jesús Zazueta</li>
-					<li><a href="http://science.webhostinggeeks.com/c-omot-za-opengl" title="by Anja Skrba at Webhostinggeeks.com">Serbo-Croatian translation</a> by Anja Skrba</li>
+					<li><a href="http://science-lakes.com/article78.html">Serbo-Croatian translation</a> by Anja Skrba</li>
 					<li><a href="http://onlygraphix.com/2014/01/18/oglplus-tutorialdeferred-renderer/" title="by Michael Ivanov at http://onlygraphix.com/">Tutorial: Deferred renderer</a> by Michael Ivanov</li>
 				</ul>
 			</div>
