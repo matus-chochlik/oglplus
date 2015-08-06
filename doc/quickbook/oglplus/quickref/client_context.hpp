24279c37bda790228b9c7bf02812558a6249d0be
Matus Chochlik <chochlik@gmail.com>
2015-08-06 21:11:43 +0200

    Updated the quickbook docs

---
+++ doc/quickbook/oglplus/quickref/client_context.hpp
@@ -0,0 +1,15 @@
+/*
+ *  Copyright 2014-2015 Matus Chochlik. Distributed under the Boost
+ *  Software License, Version 1.0. (See accompanying file
+ *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+//[oglplus_ClientContext
+
+class ClientContext
+ : public __client_BlendingOps
+ : public __client_BlendingState
+{ };
+
+//]
+
