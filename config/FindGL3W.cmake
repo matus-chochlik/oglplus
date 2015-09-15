15cb2907f1979b771952417c7ea11c2c1f8c3ab9
Jason Francis <jason@halfspiral.com>
2015-09-15 01:15:29 -0700

    Update GL3W build target

--- config/FindGL3W.cmake
+++ config/FindGL3W.cmake
@@ -2,8 +2,4 @@
 #  Software License, Version 1.0. (See accompanying file
 #  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
-if(${WIN32})
-	oglplus_common_find_module(GL3W gl3w GL/gl3w.h gl3w32)
-else()
-	oglplus_common_find_module(GL3W gl3w GL/gl3w.h GL3W)
-endif()
+oglplus_common_find_module(GL3W gl3w GL/gl3w.h gl3w)
