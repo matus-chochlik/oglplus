f5d464bb7bfef3be1e66c04419ef99a1ccbc2b9f
Matus Chochlik <chochlik@gmail.com>
2015-09-19 09:11:35 +0200

    Backporting to MESA 3D

--- source/lib/oglplus/text.cpp
+++ source/lib/oglplus/text.cpp
@@ -4,12 +4,14 @@
  *
  *  @author Matus Chochlik
  *
- *  Copyright 2010-2014 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2010-2015 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
 #include "prologue.ipp"
+#if GL_VERSION_4_1 || GL_ARB_separate_shader_objects || GL_EXT_direct_state_access
+
 #include <oglplus/string/utf8.hpp>
 #include <oglplus/string/empty.hpp>
 #include <oglplus/string/def.hpp>
@@ -34,6 +36,8 @@
 #include <oglplus/text/stb_truetype.hpp>
 #if OGLPLUS_PANGO_CAIRO_FOUND
 #include <oglplus/text/pango_cairo.hpp>
+#endif
+
 #endif
 
 #include "epilogue.ipp"
