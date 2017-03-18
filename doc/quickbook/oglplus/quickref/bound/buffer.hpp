f30569a24ebe24b4a7e386147f467b66276e49b8
Matus Chochlik <chochlik@gmail.com>
2017-03-18 06:59:21 +0100

    Multiple changes
    - Added some GL_VERSION check ifdefs.
    - Re-generated some of the auto-generated files.
    - Other minor changes.

--- doc/quickbook/oglplus/quickref/bound/buffer.hpp
+++ doc/quickbook/oglplus/quickref/bound/buffer.hpp
@@ -2,7 +2,7 @@
 /*
  *  Automatically generated file, do not edit manually!
  *
- *  Copyright 2010-2015 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2010-2017 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
