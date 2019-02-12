51792cd5bf71e7020a519e706f0edc72325ffd74
Matus Chochlik <chochlik@gmail.com>
2019-02-12 20:32:22 +0100

    Minor change in the web index page

--- web/index.php
+++ web/index.php
@@ -67,7 +67,6 @@
 				<hr class="top-div"/>
 				<!-- AddThis Button BEGIN -->
 				<div id="addthis-box" class="addthis_toolbox addthis_default_style ">
-				<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
 				<a class="addthis_button_tweet"></a>
 				<a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
 				<a class="addthis_counter addthis_pill_style"></a>
