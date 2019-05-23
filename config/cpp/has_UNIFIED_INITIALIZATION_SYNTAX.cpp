ace21f5c5cda49c088d366b9a988a304268b51b8
Matus Chochlik <chochlik@gmail.com>
2019-05-23 06:33:06 +0200

    Removed void from some empty function parameter lists

--- config/cpp/has_UNIFIED_INITIALIZATION_SYNTAX.cpp
+++ config/cpp/has_UNIFIED_INITIALIZATION_SYNTAX.cpp
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2010-2013 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2010-2019 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
@@ -7,20 +7,17 @@
  */
 #include <string>
 
-struct uis
-{
-	int i;
-	float f;
-	std::string s;
+struct uis {
+    int i;
+    float f;
+    std::string s;
 };
 
-inline uis make_uis(void)
-{
-	return {123, 456.789f, "0"};
+inline uis make_uis() {
+    return {123, 456.789f, "0"};
 }
 
-int main(int argc, const char** argv)
-{
-	uis x = make_uis();
-	return (x.i == 123)?0:1;
+int main(int argc, const char** argv) {
+    uis x = make_uis();
+    return (x.i == 123) ? 0 : 1;
 }
