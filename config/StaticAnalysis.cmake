88ae6bece0ea50c47ed8245331ea26804e2f5189
Matus Chochlik <chochlik@gmail.com>
2019-06-05 19:31:09 +0200

    First part of clang-tidy checks

---
+++ config/StaticAnalysis.cmake
@@ -0,0 +1,13 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#   http://www.boost.org/LICENSE_1_0.txt
+#
+find_program(
+	CLANG_TIDY_PROG
+	NAMES clang-tidy-7 clang-tidy
+	DOC "Path to clang-tidy executable"
+)
+
+set(INVOKE_CLANG_TIDY "${CLANG_TIDY_PROG}")
+
